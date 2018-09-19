39e552818daf68e1ac1c8c4876f65740c0354792
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-09-19 15:49:51 +0900

    add Fruit XML Parsing Dict

--- Fruit XML Parsing Dict/ViewController.swift
+++ Fruit XML Parsing Dict/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
 
+      // 딕셔너리의 배열 저장 : item의 배열
+      var elements:[[String:String]] = []
+      // 딕셔너리 : item
+      var item:[String:String] = [:]
+      
+      var currentElement = ""
+      let strURL = "http://api.androidhive.info/pizza/?format=xml"
+      
       override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
+            
+            if URL(string: strURL) != nil {
+                  if let myParser = XMLParser(contentsOf: URL(string: strURL)!) {
+                       myParser.delegate = self
+                        if myParser.parse() {
+                              print("파싱 성공")
+                              print(elements)
+                        } else {
+                              print("파싱 실패")
+                        }
+                  }
+            } else {
+                  print("URL 오류 발생!")
+            }
+            
+//            if let path = Bundle.main.url(forResource: "Fruit", withExtension: "xml") {
+//                  if let myParser = XMLParser(contentsOf: path) {
+//                        myParser.delegate = self
+//                        if myParser.parse() {
+//                            print("parse 성공")
+//                        } else {
+//                              print("파싱 오류2")
+//                        }
+//                  } else {
+//                        print("파싱 오류1")
+//                  }
+//            } else {
+//                  print("XML file 없음")
+//            }
       }
-
-      override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+      // XMLParser Delegate 메소드
+      func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+            currentElement = elementName
+            print(elementName)
       }
-
-
+      
+      func parser(_ parser: XMLParser, foundCharacters string: String) {
+            let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            
+            if !data.isEmpty {
+                  item[currentElement] = data
+                  print(item[currentElement])
+            }
+      }
+      
+      func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+            if elementName == "item" {
+                  elements.append(item)
+            }
+      }
 }
+
 
